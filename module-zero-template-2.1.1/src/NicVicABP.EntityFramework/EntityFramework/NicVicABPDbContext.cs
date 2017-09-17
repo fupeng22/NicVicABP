ee20750dfb80fbca5f650ff04aba22d0d44d7da7
fupeng22 <509243490@qq.com>
2017-09-17 10:04:56 +0800

    Initial commit

---
+++ module-zero-template-2.1.1/src/NicVicABP.EntityFramework/EntityFramework/NicVicABPDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.Common;
+using Abp.Zero.EntityFramework;
+using NicVicABP.Authorization.Roles;
+using NicVicABP.Authorization.Users;
+using NicVicABP.MultiTenancy;
+
+namespace NicVicABP.EntityFramework
+{
+    public class NicVicABPDbContext : AbpZeroDbContext<Tenant, Role, User>
+    {
+        //TODO: Define an IDbSet for your Entities...
+
+        /* NOTE: 
+         *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
+         *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
+         *   pass connection string name to base classes. ABP works either way.
+         */
+        public NicVicABPDbContext()
+            : base("Default")
+        {
+
+        }
+
+        /* NOTE:
+         *   This constructor is used by ABP to pass connection string defined in NicVicABPDataModule.PreInitialize.
+         *   Notice that, actually you will not directly create an instance of NicVicABPDbContext since ABP automatically handles it.
+         */
+        public NicVicABPDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        //This constructor is used in tests
+        public NicVicABPDbContext(DbConnection existingConnection)
+         : base(existingConnection, false)
+        {
+
+        }
+
+        public NicVicABPDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+         : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+    }
+}
---
+++ module-zero-template-2.1.1/src/NicVicABP.Web/App_Start/NicVicABPWebModule.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Abp.Hangfire;
+using Abp.Hangfire.Configuration;
+using Abp.Modules;
+using Abp.Web.Mvc;
+using Abp.Web.SignalR;
+using Abp.Zero.Configuration;
+using NicVicABP.Api;
+using Hangfire;
+
+namespace NicVicABP.Web
+{
+    [DependsOn(
+        typeof(NicVicABPDataModule),
+        typeof(NicVicABPApplicationModule),
+        typeof(NicVicABPWebApiModule),
+        typeof(AbpWebSignalRModule),
+        //typeof(AbpHangfireModule), - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
+        typeof(AbpWebMvcModule))]
+    public class NicVicABPWebModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            //Enable database based localization
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            //Configure navigation/menu
+            Configuration.Navigation.Providers.Add<NicVicABPNavigationProvider>();
+
+            //Configure Hangfire - ENABLE TO USE HANGFIRE INSTEAD OF DEFAULT JOB MANAGER
+            //Configuration.BackgroundJobs.UseHangfire(configuration =>
+            //{
+            //    configuration.GlobalConfiguration.UseSqlServerStorage("Default");
+            //});
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
